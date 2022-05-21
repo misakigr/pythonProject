3ddebf250cbceae4be03198dc26076d237f5759b
misakigr <misakigr@mail.ru>
2022-05-21 11:11:13 +0300

    Пинг пока не работает

--- Ping.py
+++ Ping.py
@@ -1,13 +1,11 @@
 import subprocess
 import platform
-try:
-    def ping(host):
+
+def ping(host):
         param = '-n' if platform.system().lower() == 'windows' else '-c'
         command = ['ping', param, '2', host]
         return subprocess.call(command)
 
-    host = 'google.com'
-    print(ping(host))
-    os.remove(t)
-except:
-    pass
+host = 'google.com'
+print(ping(host))
+#os.remove(t)
